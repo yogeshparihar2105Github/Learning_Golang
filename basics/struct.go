b7201fc2060324fa7248cac16f416a4f45854a64
Yogesh Parihar <yogeshparihar2105@gmail.com>
2025-01-05 18:35:06 +0530

    added the sturct.go

---
+++ basics/struct.go
@@ -0,0 +1,18 @@
+package basics
+
+import "fmt"
+
+type Vertex struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
+func Structss() {
+	fmt.Println(v1, p, v2, v3)
+}
